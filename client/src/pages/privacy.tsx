31aee5c769924ceb0c60e03b76311c5b32a96038
gerrardsarah <gerrardsarah@gmail.com>
2023-02-18 14:39:48 -0800

    added privacy policy page

---
+++ client/src/pages/privacy.tsx
@@ -0,0 +1,22 @@
+export default function Privacy() {
+  return (
+    <section className="w-8/12 pl-auto mx-auto py-10">
+      <h1 className="text-4xl font-bold text-center pb-2">Privacy Policy</h1>
+      <p className="text-center pb-10 text-gray-500">Last updated: February 18, 2023</p>
+      <ul className="list-disc list-inside">
+        <li className="pb-2">
+            Under no circumstances will we sell or distribute your data to third parties unless you have explicitly asked us to.
+        </li>
+        <li className="pb-2">
+            Your email will only be used as a way to register and login to your account and for no other purpose.
+        </li>
+        <li className="pb-2">
+            We will not send you any emails unless you have explicitly asked us to.
+        </li>
+        <li className="pb-2">
+            If you have any questions or concerns about our privacy policy, please contact us at <a href="https://trackmyfunds.sarahgerrard.me/contact" className="text-blue-500">trackmyfunds.sarahgerrard.me/contact</a>.
+        </li>
+    </ul>
+    </section>
+  );
+}
