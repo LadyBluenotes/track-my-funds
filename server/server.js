21e78bb53f8d0eeb1416231ba22c52c0ba08cd7d
gerrardsarah <gerrardsarah@gmail.com>
2023-02-21 10:17:12 -0800

    server?

--- server/server.js
+++ server/server.js
@@ -3,13 +3,10 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { connect } = require('http2');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
-const client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,28 +15,13 @@ app.use(require('./api/routes/expense.routes'));
 app.use(require('./api/routes/income.routes'));
 app.use(require('./api/routes/user.routes'));
 
-mongoose.set('strictQuery', false);
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI);
 
 app.get('/', (req, res) => {
-  res.send('Connected to server.');
-});
+    res.send('Welcome to the server.');
+  });  
 
-connectDB().then(() =>{
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
