7691e6746c08ccca5e9900e6104f82e4a5ab5d3b
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2023-03-11 15:54:31 -0800

    pull info to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import ProtectedPage from "@/pages/components/ProtectedPage";
 
@@ -15,6 +15,40 @@ interface monthlyExpenses {
 }
 
 export default function Dashboard() {
+  const [monthlyIncomes, setMonthlyIncomes] = useState<monthlyIncomes[] | null>(
+    null
+  );
+  const [monthlyExpenses, setMonthlyExpenses] = useState<
+    monthlyExpenses[] | null
+  >(null);
+
+  useEffect(() => {
+    fetch("/api/income/showIncomes", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMonthlyIncomes(data);
+      })
+      .catch((err) => console.log(err));
+
+    fetch("http://localhost:3000/api/expenses/showExpenses", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMonthlyExpenses(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const months = [
     "January",
@@ -33,102 +67,62 @@ export default function Dashboard() {
   const tableRows: any = [];
   const usedMonths: any = [];
 
-  const monthlyIncomes = [
-    { name: "Salary", amount: 5000, month: 1, year: 2021 },
-    { name: "Side Hustle", amount: 0, month: 1, year: 2021 },
-    { name: "Investments", amount: 100, month: 1, year: 2021 },
-    { name: "Salary", amount: 5000, month: 2, year: 2021 },
-    { name: "Side Hustle", amount: 100, month: 2, year: 2021 },
-    { name: "Investments", amount: 200, month: 2, year: 2021 },
-    { name: "Salary", amount: 3000, month: 3, year: 2021 },
-    { name: "Side Hustle", amount: 200, month: 3, year: 2021 },
-    { name: "Investments", amount: 10, month: 3, year: 2021 },
-    { name: "Salary", amount: 2500, month: 4, year: 2021 },
-    { name: "Side Hustle", amount: 150, month: 4, year: 2021 },
-    { name: "Investments", amount: 200, month: 4, year: 2021 },
-    { name: "Salary", amount: 5000, month: 1, year: 2023 },
-    { name: "Side Hustle", amount: 0, month: 1, year: 2023 },
-    { name: "Investments", amount: 100, month: 1, year: 2023 },
-    { name: "Salary", amount: 5000, month: 2, year: 2023 },
-    { name: "Side Hustle", amount: 100, month: 2, year: 2023 },
-    { name: "Investments", amount: 200, month: 3, year: 2023 },
-    { name: "Salary", amount: 3000, month: 4, year: 2023 },
-    { name: "Side Hustle", amount: 200, month: 4, year: 2023 },
-    { name: "Investments", amount: 10, month: 4, year: 2023 },
-  ];
+  if (monthlyIncomes && monthlyExpenses) {
+    const monthlyTotalIncome = (month: any, year: any) => {
+      let total = 0;
+      monthlyIncomes.forEach((income: monthlyIncomes) => {
+        if (income.month === month && income.year === year) {
+          total += income.amount;
+        }
+      });
+      return total;
+    };
 
-  const monthlyExpenses = [
-    { name: "Rent", amount: 2000 },
-    { name: "Food", amount: 500 },
-    { name: "Utilities", amount: 300 },
-    { name: "Transportation", amount: 200 },
-    { name: "Entertainment", amount: 100 },
-    { name: "Misc", amount: 100 },
-  ];
+    const remaining = (month: any, year: any) => {
+      let total: any = monthlyTotalIncome(month, year);
+      monthlyExpenses.forEach((expense: monthlyExpenses) => {
+        total -= expense.amount;
+      });
+      return total;
+    };
 
-  const tableHeaders: any = (
-    <tr>
-      <th className="px-0 py-3 text-sm w-[120px]">Month</th>
-      <th className="px-3 py-3 text-sm">Total Income</th>
-      {monthlyExpenses.map((expense: monthlyExpenses) => (
-        <th className="px-3 py-3 text-sm">{expense.name}</th>
-      ))}
-      <th className="px-3 py-3 text-sm">Remaining</th>
-    </tr>
-  );
+    monthlyIncomes.map((income: monthlyIncomes) => {
+      let monthName = months[income.month - 1];
+      let month = income.month;
+      let year = income.year;
 
-  const monthlyTotalIncome = (month: any, year: any) => {
-    let total = 0;
-    monthlyIncomes.forEach((income: monthlyIncomes) => {
-      if (income.month === month && income.year === year) {
-        total += income.amount;
-      }
-    });
-    return total;
-  };
-
-  const remaining = (month: any, year: any) => {
-    let total: any = monthlyTotalIncome(month, year);
-    monthlyExpenses.forEach((expense: monthlyExpenses) => {
-      total -= expense.amount;
-    });
-    return total;
-  };
-
-  monthlyIncomes.map((income: monthlyIncomes) => {
-    let monthName = months[income.month - 1];
-    let month = income.month;
-    let year = income.year;
-
-    if (!usedMonths.includes(`${monthName} ${year}`)) {
-      usedMonths.push(`${monthName} ${year}`);
-      tableRows.push(
-        <tr className="border-b border-gray-200">
-          <td className="px-3 py-3 text-left">
-            {monthName} {year}
-          </td>
-          <td className="px-3 py-3 text-center bg-green-100">
-            ${monthlyTotalIncome(month, year)}
-          </td>
-          {monthlyExpenses.map((expense: monthlyExpenses) => (
-            <td className="px-3 py-3 text-center bg-red-100">
-              ${expense.amount}
+      if (!usedMonths.includes(`${monthName} ${year}`)) {
+        usedMonths.push(`${monthName} ${year}`);
+        tableRows.push(
+          <tr className="border-b border-gray-200">
+            <td className="px-3 py-3 text-left">
+              {monthName} {year}
             </td>
-          ))}
-          <td className="px-3 py-3 text-right">
-            {remaining(month, year) < 0 ? (
-              <span className="text-red-500">
-                -${remaining(month, year) * -1}
-              </span>
-            ) : (
-              <span className="text-green-500">${remaining(month, year)}</span>
-            )}
-          </td>
-        </tr>
-      );
-    }
-    return tableRows;
-  });
+            <td className="px-3 py-3 text-center bg-green-100">
+              ${monthlyTotalIncome(month, year)}
+            </td>
+            {monthlyExpenses.map((expense: monthlyExpenses) => (
+              <td className="px-3 py-3 text-center bg-red-100">
+                ${expense.amount}
+              </td>
+            ))}
+            <td className="px-3 py-3 text-right">
+              {remaining(month, year) < 0 ? (
+                <span className="text-red-500">
+                  -${remaining(month, year) * -1}
+                </span>
+              ) : (
+                <span className="text-green-500">
+                  ${remaining(month, year)}
+                </span>
+              )}
+            </td>
+          </tr>
+        );
+      }
+      return tableRows;
+    });
+  }
 
   return (
     <ProtectedPage>
@@ -139,15 +133,17 @@ export default function Dashboard() {
               Your Financial Dashboard
             </caption>
             <thead className="text-xs text-gray-700 uppercase bg-grey-100 border-b-2 border-gray-400">
-              {tableHeaders}
+                <tr>
+                  <th className="px-0 py-3 text-sm w-[120px]">Month</th>
+                  <th className="px-3 py-3 text-sm">Total Income</th>
+                    <th className="px-3 py-3 text-sm">Total Expenses</th>
+                  <th className="px-3 py-3 text-sm">Remaining</th>
+                </tr>
             </thead>
             <tbody className="text-left">
               {tableRows.length === 0 ? (
                 <tr className="border-b border-gray-200">
-                  <td
-                    className="px-3 py-3 text-center"
-                    colSpan={monthlyExpenses.length + monthlyIncomes.length}
-                  >
+                  <td className="px-3 py-3 text-center" colSpan={3}>
                     No data to display
                   </td>
                 </tr>
