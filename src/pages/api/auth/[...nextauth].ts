2d6a62e6e8b4842978a0562f10b743945f4e3ffb
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2023-03-25 12:09:08 -0700

    another try

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,6 +14,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET || "",
   callbacks: {
     async session({ session, user, token }) {
       return {
