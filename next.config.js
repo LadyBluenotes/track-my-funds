bd9b981afff9fa2b580a261474fc8d4c5acded17
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2023-11-08 13:47:48 -0800

    update??

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api : {
-    bodyParser: false
+  api: {
+    bodyParser: false,
   },
   PublicRuntimeConfig: {
     MONGO_URI: process.env.MONGO_URI,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -24,6 +24,15 @@ export const authOptions: NextAuthOptions = {
         },
       };
     },
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("/")) {
+        return `${baseUrl}${url}`;
+      } else if (new URL(url).origin === baseUrl) {
+        return url;
+      } else {
+        return baseUrl;
+      }
+    },
   },
 };
 
